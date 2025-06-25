cc6268d1a8fc5ecad057f70693e807b5c545f697
maria-tang <maria.tang@ukhsa.gov.uk>
2025-06-25 16:14:35 +0100

    add model 3 with different parameters

--- scripts/run_models/run_baselinenowcast_dow.R
+++ scripts/run_models/run_baselinenowcast_dow.R
@@ -12,7 +12,6 @@ source(paste0(wd, "/scripts/run_models/functions/plotting.R"))
 source(paste0(wd, "/scripts/run_models/functions/scoring.R"))
 source(paste0(wd, "/scripts/run_models/functions/baselinenowcast_dow.R"))
 
-
 # we want to use the most recent versions of the packages on GitHub
 remotes::install_github(repo = "epinowcast/baselinenowcast")
 remotes::install_github(repo = "epinowcast/epinowcast")
@@ -27,6 +26,7 @@ set.seed(8675309)
 config <- yaml::read_yaml("./scripts/run_models/norovirus_nowcast_config.yaml")
 training_data_path <- "./outputs/data/cases_with_noise.csv"
 output_path <- "./outputs"
+plotting_output_path <- fs::dir_create(fs::path(output_path, "plots"))
 
 # will only use evaluation dates
 max_reporting_dates <- seq(from = as.Date(config$dates$tune_end_date) + lubridate::days(7),
@@ -39,26 +39,51 @@ max_reporting_dates <- seq(from = as.Date(config$dates$tune_end_date) + lubridat
 
 training_data <- vroom::vroom(training_data_path)
 
-# # # # # # # # # #
-#### FIT MODEL ####
-# # # # # # # # # #
+# # # # # # # # # # #
+#### FIT MODEL 2 ####
+# # # # # # # # # # #
 
-baselinenowcast_dow_results <- run_scripted_model(wd = wd,
+baselinenowcast_model2_results <- run_scripted_model(wd = wd,
                                               model_name = "baselinenowcast_dow",
                                               training_data = training_data,
                                               prediction_end_dates = max_reporting_dates,
                                               model_formula = "",
                                               output_columns = config$output_columns,
-                                              model_hyperparams = config$hyperparams$baselinenowcast_dow,
+                                              model_hyperparams = config$hyperparams$baselinenowcast_model2,
                                               n_pi_samples = 1000) |>
-  purrr::list_rbind()
-
-plotting_output_path <- fs::dir_create(fs::path(output_path, "plots"))
+  purrr::list_rbind() |>
+  dplyr::mutate(model = "baselinenowcast_model2")
 
 plot_nowcast(
-  data = baselinenowcast_dow_results,
+  data = baselinenowcast_model2_results,
   training_data = training_data,
-  model_name = "baselinenowcast_dow",
+  model_name = "baselinenowcast_model2",
+  plot_type = "lookbacks",
+  output_path = plotting_output_path,
+  y_limit = 150,
+  x_limit_upper = NA,
+  x_limit_lower = "2023-10-02")
+
+
+# # # # # # # # # # #
+#### FIT MODEL 3 ####
+# # # # # # # # # # #
+
+baselinenowcast_model3_results <- run_scripted_model(wd = wd,
+                                                  model_name = "baselinenowcast_dow",
+                                                  training_data = training_data,
+                                                  prediction_end_dates = max_reporting_dates,
+                                                  model_formula = "",
+                                                  output_columns = config$output_columns,
+                                                  model_hyperparams = config$hyperparams$baselinenowcast_model3,
+                                                  n_pi_samples = 1000) |>
+  purrr::list_rbind() |>
+  dplyr::mutate(model = "baselinenowcast_model3")
+
+plot_nowcast(
+  data = baselinenowcast_model3_results,
+  training_data = training_data,
+  model_name = "baselinenowcast_model3",
   plot_type = "lookbacks",
   output_path = plotting_output_path,
   y_limit = 150,
